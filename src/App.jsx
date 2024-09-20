712ff14e70183e6e2e96085a252c5151c8de99e9
Sameersingh29 <sameerchandra12@gmail.com>
2024-09-20 12:38:06 +0530

    Added B2C Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import SocialLinks from "./components/SocialLinks";
 import ScrollToTop from "./components/ScrollToTop";
+import B2CHome from "./components/LandingPage";
 
 // Importing Pages
 import Home from "./pages/Home";
@@ -14,26 +14,34 @@ import Ourfounders from "./pages/OurFounders";
 import FAQ from "./pages/FAQ";
 
 const App = () => {
+  const location = useLocation();
+  const isB2CPage = location.pathname === "/b2c";
+
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <ScrollToTop />
+    <div>
+      {!isB2CPage && <Navbar />}
+      <ScrollToTop />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact-us" element={<ContactUsPage />} />
-          <Route path="/careers" element={<Careers />} />
-          <Route path="/timeline" element={<Time />} />
-          <Route path="/ourfounders" element={<Ourfounders />} />
-          <Route path="/faq" element={<FAQ />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact-us" element={<ContactUsPage />} />
+        <Route path="/careers" element={<Careers />} />
+        <Route path="/timeline" element={<Time />} />
+        <Route path="/ourfounders" element={<Ourfounders />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/b2c" element={<B2CHome />} />
+        {/* Add more routes as needed */}
+      </Routes>
 
-          {/* Add more routes as needed */}
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+      {!isB2CPage && <Footer />}
+    </div>
   );
 };
 
-export default App;
+const AppWrapper = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AppWrapper;
