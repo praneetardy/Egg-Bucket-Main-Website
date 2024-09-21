ba4dd6e8d4dbf9d8719fc01105919131b82bc11c
GURSIMRANJEET SINGH <gursimranjeet10singh@gmail.com>
2024-09-21 15:56:51 +0000

    integerate order navbar

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Header from "../components/OrderHeader.jsx"
 // import DashBoard from "./";
 import LandingPage from "../components/LandingPage.jsx";
 
 export default function Order() {
     return (
-        // <Header/>
-    
-        <Routes>
-             <Route path="/" element={<LandingPage />} />
-            {/* <Route path="dashboard/*" element={DashBoard} /> */}
-        </Routes>
-    // <Footer/>
-);
+        <>
+            <Header />
+
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                {/* <Route path="dashboard/*" element={DashBoard} /> */}
+            </Routes>
+            {/*  <Footer /> */}
+        </>
+
+    );
 
 }
